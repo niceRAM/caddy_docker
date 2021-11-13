651b28ec94850e7966230c5b2d6a6385e93791b8
niceRAM <niceniceram@gmail.com>
2021-11-13 16:15:00 +0800

    feat: http.forwardproxy, http.filter, http.ipfilter 等插件支持; tls.dns.dnspod 替换为 tls.dns.cloudflare

--- builder.sh
+++ builder.sh
@@ -22,15 +22,16 @@ module() {
     cat > go.mod <<EOF
     module caddy
 
-    go 1.13
+    go 1.14
 
     require (
         github.com/caddyserver/caddy v1.0.5
+        github.com/caddyserver/dnsproviders v0.4.0
+        github.com/grpc-ecosystem/grpc-gateway v1.16.0
+        github.com/lucas-clemente/quic-go v0.19.3
         github.com/captncraig/caddy-realip v0.0.0-20190710144553-6df827e22ab8
-        github.com/captncraig/cors v0.0.0-20190703115713-e80254a89df1
         github.com/echocat/caddy-filter v0.14.0
         github.com/epicagency/caddy-expires v1.1.1
-        github.com/go-acme/lego/v3 v3.7.0
         github.com/hacdias/caddy-minify v1.0.2
         github.com/nicolasazrak/caddy-cache v0.3.4
         github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
@@ -41,20 +42,11 @@ EOF
     package main
 
     import (
-        "net/http"
-        "regexp"
-        "strconv"
-        "strings"
-        "github.com/caddyserver/caddy"
         "github.com/caddyserver/caddy/caddy/caddymain"
-        "github.com/caddyserver/caddy/caddyhttp/httpserver"
-        "github.com/captncraig/cors"
-        // plug in plugins here, for example:
-        // _ "import/path/here"
-        "errors"
 
-        "github.com/caddyserver/caddy/caddytls"
-        "github.com/go-acme/lego/v3/providers/dns/dnspod"
+        _ "github.com/caddyserver/dnsproviders/cloudflare"
+        _ "github.com/caddyserver/forwardproxy"                 //http.forwardproxy
+        _ "github.com/pyed/ipfilter"                            //http.ipfilter
         _ "github.com/echocat/caddy-filter"
         _ "github.com/nicolasazrak/caddy-cache"
         _ "github.com/hacdias/caddy-minify"
@@ -63,158 +55,8 @@ EOF
     )
 
     func main() {
-        caddytls.RegisterDNSProvider("dnspod", NewDNSProvider)
-        caddy.RegisterPlugin("cors", caddy.Plugin{
-            ServerType: "http",
-            Action:     setup,
-        })
-        // optional: disable telemetry
         caddymain.EnableTelemetry = false
         caddymain.Run()
-    }
-
-    func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
-        switch len(credentials) {
-        case 0:
-            return dnspod.NewDNSProvider()
-        case 1:
-            config := dnspod.NewDefaultConfig()
-            config.LoginToken = credentials[0]
-            return dnspod.NewDNSProviderConfig(config)
-        default:
-            return nil, errors.New("invalid credentials length")
-        }
-    }
-
-    type corsRule struct {
-        Conf *cors.Config
-        Path string
-    }
-
-    func setup(c *caddy.Controller) error {
-        rules, err := parseRules(c)
-        if err != nil {
-            return err
-        }
-        siteConfig := httpserver.GetConfig(c)
-        siteConfig.AddMiddleware(func(next httpserver.Handler) httpserver.Handler {
-            return httpserver.HandlerFunc(func(w http.ResponseWriter, r *http.Request) (int, error) {
-                for _, rule := range rules {
-                    if httpserver.Path(r.URL.Path).Matches(rule.Path) {
-                        rule.Conf.HandleRequest(w, r)
-                        if cors.IsPreflight(r) {
-                            return 200, nil
-                        }
-                        break
-                    }
-                }
-                return next.ServeHTTP(w, r)
-            })
-        })
-        return nil
-    }
-
-    func parseRules(c *caddy.Controller) ([]*corsRule, error) {
-        rules := []*corsRule{}
-
-        for c.Next() {
-            rule := &corsRule{Path: "/", Conf: cors.Default()}
-            args := c.RemainingArgs()
-
-            anyOrigins := false
-            if len(args) > 0 {
-                rule.Path = args[0]
-            }
-            for i := 1; i < len(args); i++ {
-                if !anyOrigins {
-                    rule.Conf.AllowedOrigins = nil
-                }
-                rule.Conf.AllowedOrigins = append(rule.Conf.AllowedOrigins, strings.Split(args[i], ",")...)
-                anyOrigins = true
-            }
-            for c.NextBlock() {
-                switch c.Val() {
-                case "origin":
-                    if !anyOrigins {
-                        rule.Conf.AllowedOrigins = nil
-                    }
-                    args := c.RemainingArgs()
-                    for _, domain := range args {
-                        rule.Conf.AllowedOrigins = append(rule.Conf.AllowedOrigins, strings.Split(domain, ",")...)
-                    }
-                    anyOrigins = true
-                case "origin_regexp":
-                    if arg, err := singleArg(c, "origin_regexp"); err != nil {
-                        return nil, err
-                    } else {
-                        r, err := regexp.Compile(arg)
-
-                        if err != nil {
-                            return nil, c.Errf("could no compile regexp: %s", err)
-                        }
-
-                        if !anyOrigins {
-                            rule.Conf.AllowedOrigins = nil
-                            anyOrigins = true
-                        }
-
-                        rule.Conf.OriginRegexps = append(rule.Conf.OriginRegexps, r)
-                    }
-                case "methods":
-                    if arg, err := singleArg(c, "methods"); err != nil {
-                        return nil, err
-                    } else {
-                        rule.Conf.AllowedMethods = arg
-                    }
-                case "allow_credentials":
-                    if arg, err := singleArg(c, "allow_credentials"); err != nil {
-                        return nil, err
-                    } else {
-                        var b bool
-                        if arg == "true" {
-                            b = true
-                        } else if arg != "false" {
-                            return nil, c.Errf("allow_credentials must be true or false.")
-                        }
-                        rule.Conf.AllowCredentials = &b
-                    }
-                case "max_age":
-                    if arg, err := singleArg(c, "max_age"); err != nil {
-                        return nil, err
-                    } else {
-                        i, err := strconv.Atoi(arg)
-                        if err != nil {
-                            return nil, c.Err("max_age must be valid int")
-                        }
-                        rule.Conf.MaxAge = i
-                    }
-                case "allowed_headers":
-                    if arg, err := singleArg(c, "allowed_headers"); err != nil {
-                        return nil, err
-                    } else {
-                        rule.Conf.AllowedHeaders = arg
-                    }
-                case "exposed_headers":
-                    if arg, err := singleArg(c, "exposed_headers"); err != nil {
-                        return nil, err
-                    } else {
-                        rule.Conf.ExposedHeaders = arg
-                    }
-                default:
-                    return nil, c.Errf("Unknown cors config item: %s", c.Val())
-                }
-            }
-            rules = append(rules, rule)
-        }
-        return rules, nil
-    }
-
-    func singleArg(c *caddy.Controller, desc string) (string, error) {
-        args := c.RemainingArgs()
-        if len(args) != 1 {
-            return "", c.Errf("%s expects exactly one argument", desc)
-        }
-        return args[0], nil
     }
 EOF
 
